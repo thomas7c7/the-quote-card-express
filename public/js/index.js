68e5cb7b638586ef63fb9e34110aba8505846b8b
thomaslimbu <Thomaslimbu102@gmail.com>
2026-02-18 14:01:42 -0500

    Made changes

---
+++ public/js/index.js
@@ -0,0 +1,39 @@
+"use strict"
+
+const elements = {
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+};
+
+const quotes = [
+    {
+        quote: "All hands! Abandon ship!",
+        author: "Captain Picard",
+    },
+
+    {
+        quote: "Doh!",
+        author: "Homer Simpson",
+    },
+
+    {
+        quote: "They hate me because i am too good",
+        author: "Cristiano Ronaldo",
+    }
+]
+
+function loopThroughQuotes() {
+    let quoteIndex = 0;
+    setInterval(() => {
+        if (quoteIndex < quotes.length) {
+            elements.quote.textContent = quotes[quoteIndex].quote;
+            elements.author.textContent = quotes[quoteIndex].author;
+            quoteIndex++;
+        } else {
+            quoteIndex = 0;
+        }
+    }, 3000);
+}
+
+setTimeout(loopThroughQuotes, 3000);
+
