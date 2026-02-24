88e1cdcdb420e9da3d06212fb2b4ae0402bee3c6
thomaslimbu <Thomaslimbu102@gmail.com>
2026-02-24 17:35:24 -0500

    Add Unsplash API background image support

--- public/js/index.js
+++ public/js/index.js
@@ -3,37 +3,21 @@
 const elements = {
     quote: document.getElementById("quote"),
     author: document.getElementById("author"),
+    background: document.querySelector(".background-img")
+
 };
 
-const quotes = [
-    {
-        quote: "All hands! Abandon ship!",
-        author: "Captain Picard",
-    },
+async function getRandomImage() {
+    const client_id = "YOUR_ACCESS_KEY";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
 
-    {
-        quote: "Doh!",
-        author: "Homer Simpson",
-    },
-
-    {
-        quote: "They hate me because i am too good",
-        author: "Cristiano Ronaldo",
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error);
     }
-]
-
-function loopThroughQuotes() {
-    let quoteIndex = 0;
-    setInterval(() => {
-        if (quoteIndex < quotes.length) {
-            elements.quote.textContent = quotes[quoteIndex].quote;
-            elements.author.textContent = quotes[quoteIndex].author;
-            quoteIndex++;
-        } else {
-            quoteIndex = 0;
-        }
-    }, 3000);
 }
-
-setTimeout(loopThroughQuotes, 3000);
-
