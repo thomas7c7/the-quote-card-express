8b7855ee11fa56c8fb01d4bd54e8120967ae767a
thomaslimbu <Thomaslimbu102@gmail.com>
2026-03-01 13:04:07 -0500

    Add Express server and Unsplash random image API integration

--- public/js/index.js
+++ public/js/index.js
@@ -8,16 +8,17 @@ const elements = {
 };
 
 async function getRandomImage() {
-    const client_id = "YOUR_ACCESS_KEY";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
     try {
         const response = await fetch(endpoint);
         const returnedData = await response.json();
-        const receivedPhotoUrl = returnedData.urls.regular;
+        const receivedPhotoUrl = returnedData.data;
 
         const imgDiv = document.querySelector(".background-img");
-        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
     } catch (error) {
         console.error(error);
     }
 }
+
+getRandomImage();
